9a3625e6a014869c79b17170d929f08b971a8f53
masterkai <kai0919@hotmail.com>
2017-04-11 13:31:50 +0800

    carousel section completed.

--- js/main.js
+++ js/main.js
@@ -131,4 +131,144 @@ jQuery(document).ready(function($){
 			navigation.insertAfter('.cd-main-content');
 		}
 	}
+	
+	
+	
+	// killcarousel setting
+	
+	$('#coverflow').KillerCarousel({
+		frontItemIndex: 0,
+		fadeEdgeItems: true,
+		autoChangeDirection: 1,
+		autoChangeDelay: 1000,
+		infiniteLoop: true,
+		// Bottom shadows on.
+		showShadow: false,
+		// Image reflections on.
+		showReflection: false,
+		// Scale at 100% of parent element.
+		
+		
+		// Choose 'flow' style rendering modes for 3d and 2d.
+		
+		animStopCallback: function () {
+			clearInterval(this.animating);
+			getAlt();
+			getData();
+		},
+		animStartCallback: function () {
+			this.animating = setInterval(function () {
+				getAlt()
+			}, 100);
+			this.animating = setInterval(function () {
+				getData()
+			}, 100);
+		},
+		profiles:[
+			{
+				minWidth:0, maxWidth: 480,
+				horizon: 'top:40%',
+				renderer3d: 'render3dFlow',
+				renderer2d: 'render2dFlow',
+				width:300,
+				// Item spacing in 3d (CSS3 3d) mode.
+				spacing3d: 140,
+				// Item spacing in 2d (no CSS3 3d) mode. 
+				spacing2d: 180,
+				autoScale: 50,
+				scale:50
+			},
+			{
+				minWidth:481, maxWidth: 650,
+				horizon: 'top:40%',
+				renderer3d: 'render3dFlow',
+				renderer2d: 'render2dFlow',
+				// Width of carousel.
+				width: 450,
+				// Item spacing in 3d (CSS3 3d) mode.
+				spacing3d: 120,
+				// Item spacing in 2d (no CSS3 3d) mode. 
+				spacing2d: 180,
+				autoScale: 50
+			},
+			{
+				minWidth:651, maxWidth: 990,
+				horizon: 'top:30%',
+				renderer3d: 'render3dFlow',
+				renderer2d: 'render2dFlow',
+				// Width of carousel.
+				width: 450,
+				// Item spacing in 3d (CSS3 3d) mode.
+				spacing3d: 120,
+				// Item spacing in 2d (no CSS3 3d) mode. 
+				spacing2d: 180,
+				autoScale: 40
+			},
+			{
+				minWidth:991, maxWidth: 1200,
+				horizon: 'top:25%',
+				renderer3d: 'render3dFlow',
+				renderer2d: 'render2dFlow',
+				// Width of carousel.
+				width: 650,
+				// Item spacing in 3d (CSS3 3d) mode.
+				spacing3d: 140,
+				// Item spacing in 2d (no CSS3 3d) mode. 
+				spacing2d: 180,
+				autoScale: 50
+			},
+			{
+				minWidth:1201, maxWidth: 1450,
+				horizon: 'top:20%',
+				renderer3d: 'render3dFlow',
+				renderer2d: 'render2dFlow',
+				// Width of carousel.
+				width: 800,
+				// Item spacing in 3d (CSS3 3d) mode.
+				spacing3d: 140,
+				// Item spacing in 2d (no CSS3 3d) mode. 
+				spacing2d: 180,
+				autoScale:40
+			},
+			{
+				minWidth:1451, maxWidth: 99999,
+				horizon: 'top:13%',
+				renderer3d: 'render3dFlow',
+				renderer2d: 'render2dFlow',
+				// Width of carousel.
+				width: 800,
+				// Item spacing in 3d (CSS3 3d) mode.
+				spacing3d: 140,
+				// Item spacing in 2d (no CSS3 3d) mode. 
+				spacing2d: 180,
+				autoScale:40
+			}
+		]
+	});
+
+	$('#next').on('click', function () {
+		$('#coverflow').data('KillerCarousel').animateToRelativeItem(1);
+	});
+	$('#prev').on('click', function () {
+		$('#coverflow').data('KillerCarousel').animateToRelativeItem(-1);
+	});
+
+	var getAlt = function (evt) {
+		var kc = $('#coverflow').data('KillerCarousel');
+		var i = kc.getFrontItemIndex();
+		var $el = kc.getItemElement(i);
+		var $img = $el.find('[alt]');
+		$('#output').text($img.attr('alt'));
+	};
+	getAlt();
+	
+	var getData = function (evt) {
+		var kc = $('#coverflow').data('KillerCarousel');
+		var i = kc.getFrontItemIndex();
+		var $el = kc.getItemElement(i);
+		var $img = $el.find('[data]');
+		$('#output2').text($img.attr('data'));
+	};
+	getData();
+	
 });
